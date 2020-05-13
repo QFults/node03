aa9af9a223a1ebbd19e43ff94eb39af76108c9b0
Quinton Fults <coding.banjo@gmail.com>
2020-05-13 10:51:30 -0700

    code

---
+++ app.js
@@ -0,0 +1,46 @@
+const nums = [1, 2, 3, 4, 5]
+
+// Run code for each value in the array
+
+// nums.forEach(num => {
+//   console.log(num)
+// })
+
+// Generate a new array of values from a given array
+
+// let numStrings = nums.map(num => {
+//   return `${num}`
+// })
+
+// Filter out values from an array based on certain criteria
+
+// let numsFiltered = nums.filter(num => {
+//   return num < 3
+// })
+
+// Derive some form of total from the overall array values
+
+// let total = nums.reduce((total, num) => {
+//   return total + num
+// })
+
+// Sort an array from least to greatest or visa versa
+
+// let numsSorted = nums.sort((a, b) => {
+//   return a - b
+// })
+
+// Safe and reliable recursive function with an end point
+
+// let count = 0
+
+// const myfunction = () => {
+
+//   if (count < 5) {
+//     count++
+//     console.log('recursion')
+//     myfunction()
+//   }
+// }
+
+// myfunction()
